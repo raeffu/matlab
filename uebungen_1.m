220a7f3de301334fd834b87af8437607625e8452
raeffu <raeffu@raeffu.ch>
2015-03-18 15:37:33 +0100

    format

--- uebungen_1.m
+++ uebungen_1.m
@@ -13,8 +13,8 @@ c1 = nthroot(123, 3) + log(600)/4
 d1 = cos((7*pi)/9) + tan(((7*pi)/15)*pi) * sind(15)
 
 %% 2a)
-x=5.2;
-y=3.6;
+x = 5.2;
+y = 3.6;
 
 a2 = (5/4)*x*y - (8*x)/y^2 + sqrt(x*y)
 %% b)
@@ -25,18 +25,18 @@ b2 = 4*(x*y)^2 - (x+y)/((2*x-y)^2) + sqrt((x+y)/(2*x-y))
 %% ...
 
 %% * 9)
-aa=[zeros(1,5) 4.8] % Zeilenvektor
+aa = [zeros(1,5) 4.8] % Zeilenvektor
 %aa2=[zeros(5,1); 4.8] % Spaltenvektor
 
 %% * 11)
-AA=[130:-20:10;linspace(1,12,7);linspace(12,72,7)]
+AA = [130:-20:10;linspace(1,12,7);linspace(12,72,7)]
 
 %% * 12)
-%BB(:,1)=linspace(5,5,4)
-%BB(:,2)=linspace(2,2,4)
-%BB(:,3)=linspace(3,3,4)
-BB=[linspace(5,5,4);linspace(2,2,4);linspace(3,3,4)]'
+%BB(:,1) = linspace(5,5,4)
+%BB(:,2) = linspace(2,2,4)
+%BB(:,3) = linspace(3,3,4)
+BB = [linspace(5,5,4);linspace(2,2,4);linspace(3,3,4)]'
 
 %% * 15)
-DD=ones(3,2)
-AA2=[DD zeros(3,2);zeros(3,2) DD]
+DD = ones(3,2)
+AA2 = [DD zeros(3,2);zeros(3,2) DD]
