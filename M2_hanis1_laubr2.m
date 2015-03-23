fd2b1ca0a0d58d15bc1b2cf726628b21129dee47
Sebastian Häni <haeni.sebastian@gmail.com>
2015-03-23 19:44:56 +0100

    optimized display of task 8 and code cleanup

--- M2_hanis1_laubr2.m
+++ M2_hanis1_laubr2.m
@@ -1,15 +1,13 @@
 function [] = M2_hanis1_laubr2()
-%M2 This function solves problems 1, 3, 7 and 8
 %   Serie M2, hanis1 (Sebastian Haeni), laubr2 (Raphael Laubscher)
 
     clc;                         % Clear console
-    currentTask = 4;             % Current selected task
+    currentTask = 3;             % Current selected task
     f = figure('Visible','off'); % Create a figure
-    drawUi();                    % Draw control to select tasks
-    f.Visible = 'on';            % Make figure visble
     f.SizeChangedFcn = @(src, ev) drawUi(); % Redraw function
+    f.Visible = 'on';            % Make figure visble
 
-    % Create pop-up menu
+    % Draw UI
     function drawUi()
         clf;
         fPos = f.Position;
@@ -21,7 +19,7 @@ function [] = M2_hanis1_laubr2()
             'String', {'Aufgabe 1', 'Aufgabe 3', 'Aufgabe 7',...
                 'Aufgabe 8'},...
             'Position', [5 fPos(4)-50 100 50],...
-            'Callback', @(src, event)setTask(src.Value));
+            'Callback', @(src, event) setTask(src.Value));
         c.Value = currentTask;
 
         switch currentTask
@@ -36,6 +34,7 @@ function [] = M2_hanis1_laubr2()
         end
     end
 
+    % Setting current task and update UI
     function setTask(value)
         currentTask = value;
         drawUi();
@@ -65,12 +64,31 @@ function [] = M2_hanis1_laubr2()
         subplot(2, 1, 2);
         fplot(sn, [-2, 4]);
         title('-2 \leq x \leq 4');
+        
+        % setting axis is not necessary as fplot does what we want
+        % automagically
     end
 
     function task8()
+        subplot(1, 2, 1);
+        task8a();
+        subplot(1, 2, 2);
+        task8b();
+    end
+
+    function task8a()
+        % data declaration
         sitzeProPartei = [54, 46, 30, 28, 15, 12, 9, 2, 2, 1, 1];
         parteien = {'SVP', 'SP', 'FDP', 'CVP', 'GPS', 'glp',...
             'BDP', 'EVP', 'Lega', 'CSP OW', 'MCG'};
+        
+        % drawing pie chart
+        pie(sitzeProPartei, parteien);
+        title('Nationalraete pro Partei');
+    end
+
+    function task8b()
+        % data declaration
         sitzeProKanton = [34, 26, 18, 15, 12, 11, 10, 8, 7, 7, 7, 7, 6,...
             5, 5, 5, 4, 3, 2, 2, 1, 1, 1, 1, 1, 1];
         kantone = {'Zuerich', 'Bern', 'Waadt', 'Aargau', 'St. Gallen',...
@@ -79,14 +97,25 @@ function [] = M2_hanis1_laubr2()
             'Basel-Stadt', 'Graubuenden', 'Neuenburg', 'Schwyz', 'Zug',...
             'Schaffhausen', 'Jura', 'Uri', 'Obwalden', 'Nidwalden',...
             'Glarus', 'Appenzell Ausserrhoden', 'Appenzell Innerrhoden'};
-
-        subplot(1, 2, 1);
-        pie(sitzeProPartei, parteien);
-        title('Nationalraete pro Partei');
-
-        subplot(1, 2, 2);
-        pie(sitzeProKanton, kantone); % TODO choose more sensible chart
+       
+        % drawing bar chart
+        h = bar(sitzeProKanton);
         title('Nationalraete pro Kanton');
+        set(gca, 'XTickLabel', {}); % clearing X labels
+        
+        % drawing labels directly on bars
+        for i = 1:length(kantone)
+            xData = h.XData;
+            yData = h.YData;
+            text(xData(i), yData(i) + .1, kantone(i),...
+                'Color', 'k',...
+                'HorizontalAlignment', 'left',...
+                'Rotation', 90);
+        end
+        
+        % fit the graph nicely
+        ylim([0 40]);
+        xlim([0 27]);
     end
 
 end
