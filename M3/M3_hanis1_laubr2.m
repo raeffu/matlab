1eb68f106368c61ee575fab819ddd3b11e0e9acc
Sebastian Häni <haeni.sebastian@gmail.com>
2015-04-26 22:01:46 +0200

    code clean up

--- M3/M3_hanis1_laubr2.m
+++ M3/M3_hanis1_laubr2.m
@@ -59,23 +59,23 @@ function [] = M3_hanis1_laubr2()
     function task4()
         % a)
         subplot(2, 2, 1)
-        t=[0:10];
+        t=0:10;
         x=3*cos(t)+4*sin(t);
         plot(t,x,'.-')
         % b)
         subplot(2, 2, 2)
-        t=[0:0.1:10];
+        t=0:0.1:10;
         x=3*cos(t)+4*sin(t);
         plot(t,x,'.-')
         % c)
-        n=[1:4000];
+        n=1:4000;
         x=cos(2*pi*440*n/8192);
         sound(x)
         % d)
         subplot(2,2,3)
         plot(x)
         % e)
-        x(1:8)
+        x(1:8);
         % f)
         subplot(2,2,4)
         plot(x(1:100))
@@ -90,26 +90,25 @@ function [] = M3_hanis1_laubr2()
     function task5()
         % a)
         load('train', 'y');
-        whos;
-        sound(y)
+        sound(y);
         % b)
-        subplot(2,2,1)
-        plot(y)
+        subplot(2,2,1);
+        plot(y);
         n=1501:1700;
-        subplot(2,2,2)
-        plot(n, y(n), '.-')
+        subplot(2,2,2);
+        plot(n, y(n), '.-');
         % c)
-        sound(y(1:2:end))
+        sound(y(1:2:end));
         % d)
         z = y' .* cos(2*pi*1000*(1:length(y))/8192);
-        sound(z)
+        sound(z);
         % e)
-        subplot(2,2,3)
-        plot(z)
+        subplot(2,2,3);
+        plot(z);
         n=1501:1700;
-        subplot(2,2,4)
-        plot(n, z(n), '.-')
-        suptitle('Abbildung 2 von hanis1 und laubr2')
+        subplot(2,2,4);
+        plot(n, z(n), '.-');
+        suptitle('Abbildung 2 von hanis1 und laubr2');
         
         %print -dpng abbildung2_hanis1_laubr2.png
     end
